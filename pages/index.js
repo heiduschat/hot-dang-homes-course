0bfab263fb8601ab312b37841761e4a4a9e41997
Heiduschat <aheiduschat@3pc.de>
2023-01-24 17:36:32 +0100

    feat(FE): add dark mode

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <div>Next JS &amp; WordPress course.</div>;
+  return (
+  <div className="h-full bg-white dark:bg-gray-800">
+    <h1 className="text-gray-900 dark:text-white">Dark mode is here!</h1>
+    <p className="text-gray-600 dark:text-gray-300">
+      Lorem ipsum...
+    </p>
+  </div>
+);
 }
