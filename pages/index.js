ebb0e1113da1db9da05bf3692471dcccfef76bcf
Heiduschat <aheiduschat@3pc.de>
2023-01-24 18:07:56 +0100

    add graphql query for page titles

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
-export default function Home() {
+import { gql } from '@apollo/client';
+import client from 'client';
+
+export default function Home(props) {
+  console.log(props);
   return (
-  <div className="h-full bg-white dark:bg-gray-800">
-    <h1 className="text-gray-900 dark:text-white">Dark mode is here!</h1>
-    <p className="text-gray-600 dark:text-gray-300">
-      Lorem ipsum...
-    </p>
-  </div>
-);
+    <div className="h-full bg-white dark:bg-gray-800">
+      <h1 className="text-gray-900 dark:text-white">Dark mode is here!</h1>
+      <p className="text-gray-600 dark:text-gray-300">
+        Lorem ipsum...
+      </p>
+    </div>
+  )
+}
+
+export const getStaticProps = async () => {
+  const {data} = await client.query({
+    query: gql`
+    query NewQuery {
+      pages {
+        nodes {
+          title
+        }
+      }
+    }
+    `
+  })
+  return {
+    props: {
+      data,
+      myexampleprop: "foo",
+    }
+  }
 }
