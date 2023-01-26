3a74a30d5d61eb73b23e4d3ec46fc921d76bedae
Heiduschat <aheiduschat@3pc.de>
2023-01-26 15:46:07 +0100

    add font utility functions / render heading iin cover block

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -1,12 +1,29 @@
 import { Cover } from 'components/Cover';
+import { Heading } from 'components/Heading';
 
 export const BlockRenderer = ({blocks}) => {
   return blocks.map(block => {
     switch (block.name) {
-      case 'core/cover': {
-        // return <div className="h-full bg-white dark:bg-gray-800" key={block.id}>core cover</div>
-        return <Cover key={block.id} background={block.attributes.url}>core cover</Cover>
+
+      case 'core/heading': {
+        return (
+        <Heading
+          key={block.id}
+          level={block.attributes.level}
+          content={block.attributes.content}
+          textAlign={block.attributes.textAlign}
+        />
+        );
       }
+
+      case 'core/cover': {
+        return (
+          <Cover key={block.id} background={block.attributes.url}>
+            <BlockRenderer blocks={block.innerBlocks} />
+          </Cover>
+        )
+      }
+
       default:
         return null;
     }
--- components/Cover/Cover.js
+++ components/Cover/Cover.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export const Cover = ({children, background}) => {
   return (
-    <div className="relative flex justify-center items-center h-screen min-h-[400px] bg-slate-800 dark:bg-gray-800">
+    <div className="relative flex justify-center items-center h-screen min-h-[400px] text-slate-200 bg-slate-800 dark:bg-gray-800">
       <Image
         alt="Cover"
         src={background}
@@ -10,7 +10,7 @@ export const Cover = ({children, background}) => {
         objectFit="cover"
         className="mixed-blend-soft-light"
       />
-      {children}
+      <div className="max-w-5xl z-10">{children}</div>
     </div>
   )
 }
