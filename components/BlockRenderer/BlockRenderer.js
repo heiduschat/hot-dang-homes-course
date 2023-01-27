084de928fb33f690005e6ad544285b32285d6144
Heiduschat <aheiduschat@3pc.de>
2023-01-27 13:28:08 +0100

    feat(blocks/paragraph): add component / add theme color routine

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -1,5 +1,7 @@
 import { Cover } from 'components/Cover';
 import { Heading } from 'components/Heading';
+import { Paragraph } from 'components/Paragraph';
+import { theme } from 'theme';
 
 export const BlockRenderer = ({blocks}) => {
   return blocks.map(block => {
@@ -7,12 +9,26 @@ export const BlockRenderer = ({blocks}) => {
 
       case 'core/heading': {
         return (
-        <Heading
-          key={block.id}
-          level={block.attributes.level}
-          content={block.attributes.content}
-          textAlign={block.attributes.textAlign}
-        />
+          <Heading
+            key={block.id}
+            level={block.attributes.level}
+            content={block.attributes.content}
+            textAlign={block.attributes.textAlign}
+          />
+        );
+      }
+
+      case 'core/paragraph': {
+        return (
+          <Paragraph
+            key={block.id}
+            content={block.attributes.content}
+            textAlign={block.attributes.align}
+            textColor={
+              theme[block.attributes.textColor] ||
+              block.attributes.style?.color?.text
+            }
+          />
         );
       }
 
---
+++ theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+  foreground: 'canvastext',
+  background: 'canvas',
+  primary: '#1a4548',
+  secondary: '#ffe2c7',
+  tertiary: '#F6F6F6',
+  slate: '#1e293B',
+  'vivid-green-cyan': '#00d084'
+}
